2c9df56ddc79d9ea28f272577cb2a0cd15a9e990
Debin07 <61648826+Debin07@users.noreply.github.com>
2025-06-27 22:10:28 +0700

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,11 +1,3 @@
-from app import db, Admin
-from werkzeug.security import generate_password_hash
-from flask import Flask
-
-with db.app.app_context():
+from app import app, db
+with app.app_context():
     db.create_all()
-    if not Admin.query.first():
-        admin = Admin(username='Debin07', password=generate_password_hash('Pantat21'))
-        db.session.add(admin)
-        db.session.commit()
-        print("✅ Admin berhasil dibuat.")
